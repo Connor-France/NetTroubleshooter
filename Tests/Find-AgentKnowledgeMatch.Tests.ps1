35cd3947bec26e91a983b536253b6355ecf53469
Connor <connor_france@outlook.com>
2026-07-04 16:25:07 +0100

    Add DNS knowledge matcher tests

--- Tests/Find-AgentKnowledgeMatch.Tests.ps1
+++ Tests/Find-AgentKnowledgeMatch.Tests.ps1
@@ -35,3 +35,35 @@ Signal                 : 25%
         $result.PatternName | Should -Contain "Weak Wi-Fi signal"
     }
 }
+
+Describe "Find-AgentKnowledgeMatch - DNS" {
+    It "does not flag DNS as failed when DNS data exists" {
+        $fakeDnsData = [pscustomobject]@{
+            Type = "dns"
+            Dns = @(
+                [pscustomobject]@{
+                    Name      = "google.com"
+                    Type      = "A"
+                    IPAddress = "142.250.140.100"
+                }
+            )
+            Servers = @()
+        }
+
+        $result = Find-AgentKnowledgeMatch -Scenario dns -InputData $fakeDnsData
+
+        $result.PatternName | Should -Not -Contain "Default DNS lookup failure"
+    }
+
+    It "flags DNS lookup failure when DNS data is missing" {
+        $fakeDnsData = [pscustomobject]@{
+            Type    = "dns"
+            Dns     = $null
+            Servers = @()
+        }
+
+        $result = Find-AgentKnowledgeMatch -Scenario dns -InputData $fakeDnsData
+
+        $result.PatternName | Should -Contain "Default DNS lookup failure"
+    }
+}
